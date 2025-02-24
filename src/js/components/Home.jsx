b5259c99f2947392f7a5e3aa7c053dd3b455ffa8
Lasarikoo <lazaro@bega.ltd>
2025-02-24 21:57:36 +0000

    Finished

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -2,11 +2,6 @@ import React from "react";
 
 import SecondsCounter from "./SecondsCounter"
 
-
-window.onload = function () {
-    setInterval(() => SecondsCounter(100), 1000);
-};
-
 //create your first component
 const Home = () => {
 	return (
--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -4,9 +4,19 @@ import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 
 
-function SecondsCounter(seconds) {
-    let numerito = Math.floor(Math.random() * 100);
+function SecondsCounter() {
+        let seconds = 0;
 
+        setInterval(() => {
+            seconds++;
+
+            let secondsSeparated = seconds.toString().padStart(6, "0").split("");
+
+            for (let x = 0; x < 6; x++) {
+                let numberDom = document.getElementById(`number${x}`);
+                if (numberDom) numberDom.textContent = secondsSeparated[x];
+            }
+        }, 1000);
 
     return (
         <div className="container-fluid bg-dark d-flex justify-content-center">
@@ -14,22 +24,22 @@ function SecondsCounter(seconds) {
                     <h1 className="number"><FontAwesomeIcon icon={faClock} /></h1>
                 </div>
                 <div className="numberCard border rounded">
-                    <h1 className="number">{numerito}</h1>
+                    <h1 id="number0" className="number">0</h1>
                 </div>
                 <div className="numberCard border rounded">
-                    <h1 className="number">0</h1>
+                    <h1 id="number1" className="number">0</h1>
                 </div>
                 <div className="numberCard border rounded">
-                    <h1 className="number">0</h1>
+                    <h1 id="number2" className="number">0</h1>
                 </div>
                 <div className="numberCard border rounded">
-                    <h1 className="number">0</h1>
+                    <h1 id="number3" className="number">0</h1>
                 </div>
                 <div className="numberCard border rounded">
-                    <h1 className="number">0</h1>
+                    <h1 id="number4" className="number">0</h1>
                 </div>
                 <div className="numberCard border rounded">
-                    <h1 className="number">0</h1>
+                    <h1 id="number5" className="number">0</h1>
                 </div>
         </div>
     );
